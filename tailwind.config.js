57f197d598fabba1b874a5a93db28d1a810a6ed7
filipe.burgonovo <burgodev@gmail.com>
2025-03-15 19:07:15 -0300

    styles: tailwind config and globals

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "0px",
+        sm: "600px",
+        md: "1024px",
+        lg: "1440px",
+        xl: "1920px",
+        xxl: "2560px",
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeOut: 'fadeOut 0.5s ease-in-out',
+        scaleUp: 'scaleUp 0.3s ease-in-out',
+        scaleDown: 'scaleDown 0.3s ease-in-out',
+        slideInFromRightAndFadeIn: 'slideInFromRightAndFadeIn 0.5s ease-in-out',
+        slideInFromLeftAndFadeIn: 'slideInFromLeftAndFadeIn 0.5s ease-in-out',
+
+      },
+      colors: {
+        dark: '#060708',
+        darkGrey: '#181818',
+        lightGrey: '#e5e5e5',
+        white: '#ffffff',
+        mediumGrey: '#b3b3b3',
+        black: '#000000',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 0.75 },
+        },
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        scaleUp: {
+          '0%': { transform: 'scale(0.8)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        scaleDown: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(0.8)' },
+        },
+        slideInFromRightAndFadeIn: {
+          '0%': { transform: 'translateX(25%)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+        slideInFromLeftAndFadeIn: {
+          '0%': { transform: 'translateX(-25%)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
