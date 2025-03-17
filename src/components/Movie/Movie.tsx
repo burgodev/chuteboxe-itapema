06053c9c8173c60b63efd03a51b1a4a199fee589
filipe.burgonovo <burgodev@gmail.com>
2025-03-16 21:13:12 -0300

    style: movie hover improvements

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -11,7 +11,7 @@ const Movie: React.FC<MovieProps> = ({ onClick, movie }) => {
 
   return (
     <div
-      className="overflow-hidden text-white transition-transform transform border border-transparent rounded shadow-lg opacity-100 cursor-pointer hover:border-white hover:opacity-100 animate-fadeIn group"
+      className="overflow-hidden text-white transition-transform transform border border-transparent shadow-lg cursor-pointer hover:border-white animate-fadeIn group rounded-[8px]"
       onClick={onClick}
       role="button"
       tabIndex={0}
@@ -22,11 +22,15 @@ const Movie: React.FC<MovieProps> = ({ onClick, movie }) => {
       }}
       aria-label={`View details for ${title}`}
     >
-      {/* <div className="absolute inset-0"></div> */}
-      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center p-12 text-center transition-opacity bg-black bg-opacity-50 opacity-0 group-hover:opacity-100">
-        <h5 className="text-lg md:text-3xl">{title}</h5>
-        <p className="text-xs font-bold text-gray-400 md:text-lg">{genre}</p>
-        <p className="text-xs font-bold text-gray-400 md:text-lg">({year})</p>
+      <div className="absolute inset-0"></div>
+      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center p-12 text-center transition-opacity bg-black bg-opacity-35 group-hover:bg-opacity-75">
+        <h5 className="text-lg opacity-75 md:text-3xl group-hover:opacity-100">
+          {title}
+        </h5>
+        <div className="opacity-0 group-hover:opacity-100">
+          <p className="text-xs font-bold text-gray-400 md:text-lg ">{genre}</p>
+          <p className="text-xs font-bold text-gray-400 md:text-lg">({year})</p>
+        </div>
       </div>
       <img
         className="object-cover w-full h-[45vh] transition-transform transform group-hover:scale-110"
